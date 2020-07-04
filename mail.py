cec5ce98e924e0d922ca1a0a5f8e173465e62ddd
Tanya Chetna Vaish <63603952+TanyaChetnaVaish@users.noreply.github.com>
2020-07-04 20:32:42 +0530

    Create mail.py

---
+++ mail.py
@@ -0,0 +1,17 @@
+import smtplib
+import urllib.request as urllib
+# Senders email
+sender_email = "tanya.vaish.743@gmail.com"
+# Receivers email
+rec_email = ""tanya.vaish.743@gmail.com"
+
+message = "Kuberenetes cluster not running.Do the required changes."
+# Initialize the server variable
+server = smtplib.SMTP('smtp.gmail.com', 587)
+# Start the server connection
+server.starttls()
+# Login
+server.login(""tanya.vaish.743@gmail.com", "intelCORE@i56")
+print("Login Success!")
+# Send Email
+server.sendmail("Tanya Chetna Vaish", ""tanya.vaish.743@gmail.com", message)
